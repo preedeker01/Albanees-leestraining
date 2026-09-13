fef54a921c4301ae0e50725dfbe8e2b228f30c8b
preedeker01 <pligorx@gmail.com>
2026-09-13 11:48:01 +0200

    icoon update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME='albanese-leestraining-v8-750woorden';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./SpeechOn.wav','./SpeechOff.wav','./audio-map.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
+const CACHE_NAME='albanese-leestraining-v9-safari-pwa';
+const CACHE_PREFIX='albanese-leestraining-';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-180.png','./icon-192.png','./icon-512.png','./SpeechOn.wav','./SpeechOff.wav','./audio-map.json','./word-data-750.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>Promise.all(ASSETS.map(a=>c.add(a).catch(()=>null))))));
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
